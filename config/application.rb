1b3bc56f2ec4a809760479fcf147379c0f556412
Tim Prentis <tprentis@gmail.com>
2012-12-15 13:11:09 -0500

    config assets on precompile = true

--- config/application.rb
+++ config/application.rb
@@ -58,8 +58,8 @@ module Sunray
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
     
-    ActiveScaffold.js_config[:highlight] = {}
+    
   end
 end
