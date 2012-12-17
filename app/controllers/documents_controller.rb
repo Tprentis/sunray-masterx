9b830264491aa4af68eff36c27e4c84ebd3d9bce
Tim Prentis <tprentis@gmail.com>
2012-12-17 10:11:22 -0500

    Format Currency and add Margins

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -3,7 +3,7 @@ class DocumentsController < ApplicationController
     
 
     active_scaffold :document do |config|
-    #  config.theme = :gray 
+    # config.theme = :gray 
        
       config.list.page_links_inner_window = true
       config.list.sorting = { :state => :asc }
@@ -15,11 +15,14 @@ class DocumentsController < ApplicationController
        
       columns[:document_legal_name].label = "Document Legal Name"
       columns[:document_legal_name].inplace_edit = true 
- 
+      config.columns[:document_legal_name].options[:size] = 20
+      
       columns[:document_type].label = "Type Of Document"
       columns[:document_type].inplace_edit = true
       columns[:document_type].form_ui = :select 
       columns[:document_type].options[:options] = [" ", "Hard Letter", "Soft Letter"]
+      
+      config.columns[:default_amount].options[:format] = :currency
       
       config.columns = [:state, :documentparent, :document_legal_name, :document_type]
       columns[:documentparent].label = "Document Name"
@@ -28,18 +31,18 @@ class DocumentsController < ApplicationController
       
       columns[:usps_barcode].label = "USPS BarCode?"
       columns[:documentparts].label = "Document Sections"
-      columns[:signer].label = "Signer ?" 
-      columns[:add_todays_date].label = "Add Today's Date ?"      
+      columns[:signer].label = "Signer?" 
+      columns[:add_todays_date].label = "Add Today's Date?"      
       
-      columns[:use_default_amount].label = "Use Default Amount ?"      
-      columns[:soft_letter].label = "Soft Letter ?"      
-      columns[:sunray_logo].label = "Use Sunray Logo ?"      
-      columns[:add_completed_date].label = "Add Completed Date ?"      
-      columns[:to_from_address].label = "To and From Addresses ?"      
-      columns[:delivery_date_on_notice].label = "Delivery Date On Notice ?" 
-      columns[:total_amount_of_job].label = "Total Amount Of Job ?"    
-      columns[:balance_remaining].label = "Balance Ramaining ?"             
-      columns[:ariela_signature].label = "Ariela Signature ?"  
+      columns[:use_default_amount].label = "Use Default Amount?"      
+      columns[:soft_letter].label = "Soft Letter?"      
+      columns[:sunray_logo].label = "Use Sunray Logo?"      
+      columns[:add_completed_date].label = "Add Completed Date?"      
+      columns[:to_from_address].label = "To and From Addresses?"      
+      columns[:delivery_date_on_notice].label = "Delivery Date On Notice?" 
+      columns[:total_amount_of_job].label = "Total Amount Of Job?"    
+      columns[:balance_remaining].label = "Balance Ramaining?"             
+      columns[:ariela_signature].label = "Ariela Signature?"  
       
       config.nested.add_link(:documentparts).label = "Document Parts"
       
